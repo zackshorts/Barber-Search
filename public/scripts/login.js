5b959e28b893c938df646d993acdd5ad67b7d306
DESKTOP-TI81K93\gavin <gavinrob92@gmail.com>
2017-11-18 22:04:05 -0700

    stores info in database correctly

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,26 +1,29 @@
 function onSignIn(googleUser) {
-    console.log('Google Auth Response', googleUser);
-    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
     var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
         unsubscribe();
-        // Check if we are already signed-in Firebase with the correct user.
+
         if (!isUserEqual(googleUser, firebaseUser)) {
-            // Build Firebase credential with the Google ID token.
             var credential = firebase.auth.GoogleAuthProvider.credential(
-                googleUser.getAuthResponse().id_token);
-            // Sign in with credential from the Google user.
-            firebase.auth().signInWithCredential(credential).catch(function(error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                // The email of the user's account used.
-                var email = error.email;
-                // The firebase.auth.AuthCredential type that was used.
-                var credential = error.credential;
-                // ...
+                googleUser.getAuthResponse().id_token
+            );
+
+            firebase.auth().signInWithCredential(credential)
+            .catch(function(error) {
+                console.log(`error ${errorCode}: ${error.message}.`)
+            })
+            .then(function() {
+                var user = firebase.auth().currentUser;
+
+                var rootRef = firebase.database().ref();
+                var userRef = rootRef.child('barber').child(user.uid);
+                
+                userRef.set({
+                    fName: user.displayName,
+                    email: user.email,
+                    photoURL: user.photoURL
+                });
+                window.location.href = '../barber.html';
             });
-        } else {
-            console.log('User already signed-in Firebase.');
         }
     });
 }
@@ -42,6 +45,7 @@ function isUserEqual(googleUser, firebaseUser) {
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+    firebase.auth().signOut()
       console.log('User signed out.');
     });
   }
