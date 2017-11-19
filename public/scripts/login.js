c998a38317e2342759f58783cb3cd87aae4505af
Zack Shorts <shorts23@gmail.com>
2017-11-18 17:35:05 -0700

    this will now create a new firebase user and check to see if they are in the users. We need to connect to the database now.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,63 +1,43 @@
-// firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-// .then(function() {
-//     var provider = new firebase.auth.GoogleAuthProvider();
-//   // In memory persistence will be applied to the signed in Google user
-//   // even though the persistence was set to 'none' and a page redirect
-//   // occurred.
-//   window.location.href = "../main.html";
-//   return firebase.auth().signInWithRedirect(provider);
-// })
-// .catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-// });
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    //  var token = result.credential.accessToken;
-   
-    // console.log(result);
-    // var userID = result.user.ca.displayName;
-    // var user = result.user;
-    // console.log(user);
-    
-    // firebase.database().ref('barber/' + userId).set({
-    //     fname: userID
-    //   });
-
-    var rootRef = firebase.database().ref();
-    var barberRef = rootRef.child('barber');
-    var fnameRef = barberRef.push();
-    console.log(fnameRef);
-    fnameRef.set({
-      fname: "Zachary Shorts",
-      email: "shorts23@gmail.com"
+function onSignIn(googleUser) {
+    console.log('Google Auth Response', googleUser);
+    // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!isUserEqual(googleUser, firebaseUser)) {
+            // Build Firebase credential with the Google ID token.
+            var credential = firebase.auth.GoogleAuthProvider.credential(
+                googleUser.getAuthResponse().id_token);
+            // Sign in with credential from the Google user.
+            firebase.auth().signInWithCredential(credential).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+        } else {
+            console.log('User already signed-in Firebase.');
+        }
     });
+}
 
-  }).catch(function(error) {
-    // Handle Errors here.
-    console.log("test");
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    //window.location.href = "../404.html";
-    // ...
-  });
-
-// function onSignIn(googleUser) {
-//     var profile = googleUser.getBasicProfile();
-//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//     console.log('Name: ' + profile.getName());
-//     console.log('Image URL: ' + profile.getImageUrl());
-//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-//     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-//     firebase.database().ref('barber/' + profile.getId()).set({
-//         fname: profile.getName()
-//     });
-//   }
+function isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+        var providerData = firebaseUser.providerData;
+        for (var i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === googleUser.getBasicProfile().getId()) {
+                // We don't need to reauth the Firebase connection.
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
@@ -65,4 +45,3 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
       console.log('User signed out.');
     });
   }
-  
