e4952b76debee30c39515083367bed4bc071c347
DESKTOP-TI81K93\gavin <gavinrob92@gmail.com>
2017-11-28 18:55:12 -0700

    barber list's first commit

---
+++ public/scripts/barberlist.js
@@ -0,0 +1,32 @@
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function (user) {
+        console.log('hi');
+        var query = firebase.database().ref("barber").orderByKey();
+        query.once("value")
+            .then(function (snapshot) {
+                var barberList = document.querySelector("#barber-list");
+                snapshot.forEach(function (childSnapshot) {
+                    var key = childSnapshot.key;
+                    var childData = childSnapshot.val();
+
+                    var barberItem = 
+                    `<div class="barber-item">
+                        <img class="barber-photo" src="${childData.photoURL}">
+                        <div class="barber-info">
+                            <h1 class="barber-name">${childData.fName}</h1>
+                            <h2 class="barber-location">Provo, UT</h2>
+                        </div>
+                    </div>`;
+
+                    barberList.insertAdjacentHTML('beforeend', barberItem);
+                });
+            });
+    });
+    // [END authstatelistener]
+
+}
+window.onload = function () {
+    initApp();
+}
